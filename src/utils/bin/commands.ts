b6d0fd20c7c23d8f0da40782c1c75c422c199e92
unkn0wnB0y5 <unknwnby5@gmail.com>
2025-04-04 01:23:36 +0000

    Update commands12.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -5,17 +5,8 @@ import config from '../../../config.json';
 
 // Help
 export const help = async (args: string[]): Promise<string> => {
-  const commands = Object.keys(bin).sort().join(', ');
-  var c = '';
-  for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
-    if (i % 7 === 0) {
-      c += Object.keys(bin).sort()[i - 1] + '\n';
-    } else {
-      c += Object.keys(bin).sort()[i - 1] + ' ';
-    }
-  }
+  
   return `Welcome! Here are all the available commands:
-\n${c}\n
 [tab]: trigger completion.
 [ctrl+l]/clear: clear terminal.\n
 whoami 
