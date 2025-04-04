1586a94c857b82947f8622b9c9beeaae266fdf30
unkn0wnB0y5 <unknwnby5@gmail.com>
2025-04-04 01:11:40 +0000

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -1,3 +1,15 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+  if (config.ascii === 'cveinnt') {
+    return ``;
+  } else {
+    return ``;
+  }
+};
+
+export default sumfetch;
+
 
 
 
